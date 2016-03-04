0594df771ae62201a4e292c621074b30f0b0dca9
David Cramb <david.cramb@gmail.com>
2016-03-03 21:54:06 -0600

    added event listeners on hover for H1 element

--- events.js
+++ events.js
@@ -4,19 +4,24 @@ var sectionClick = document.querySelector('article');
 // adds event listener to children elements of article to show the section name of each section element
 sectionClick.addEventListener('click', showcase, false);
 
-// 
+// This function outputs the section name to the output element, ran from the event listener
 function showcase(section) {
-  console.log(section.target.innerHTML);
   var sectionShowcase = section.target.innerHTML;
-  outputEl.innerHTML = 'You clicked on the ' + sectionShowcase + 'section!'
+  outputEl.innerHTML = 'You clicked on the ' + sectionShowcase + ' section!'
+};
 
-  };
-// }
-
+// Variable grabs the H1 Element, Event Listeners activate for hovering over and out of the field.
+var h1Hover = document.querySelector('h1');
+h1Hover.addEventListener('mouseover', hoverIn, false);
+h1Hover.addEventListener('mouseout', hoverOut, false);
+// Changes outputEl text when the mouse hovers on and out of the H1 Element
+function hoverIn (h1) {
+  outputEl.innerHTML = 'You\'re in the danger zone!';
+};
+function hoverOut (h1) {
+  outputEl.innerHTML = 'You\'ve left the danger zone!';
+};
 
   
 
-// getSection(sec).addEventListener('click', function(){
-//   console.log('sec');
-// });
 
