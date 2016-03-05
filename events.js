a01e7a4f1bcf3368f86bc183a7ce6130d1212494
David Cramb <david.cramb@gmail.com>
2016-03-05 09:46:00 -0600

    final touch, made buttons block elements

--- events.js
+++ events.js
@@ -42,6 +42,7 @@ blueButton.addEventListener('click', function() {
   var blue = document.getElementById('guinea-pig');
   blue.classList.toggle('bluetext');
 });
+
 var hulkButton = document.getElementById('make-large');
 hulkButton.addEventListener('click', function() {
   var hulk = document.getElementById('guinea-pig');
@@ -53,6 +54,7 @@ captureButton.addEventListener('click', function() {
   var border = document.getElementById('guinea-pig');
   border.classList.toggle('addborder');
 });
+
 var roundButton = document.getElementById('add-rounding');
 roundButton.addEventListener('click', function() {
   var rounding = document.getElementById('guinea-pig');
