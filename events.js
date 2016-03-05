68189557a75a8a3750155776ed630bba962cfcf9
David Cramb <david.cramb@gmail.com>
2016-03-04 20:46:33 -0600

    outputEl mostly mirrors what is typed into the input element, but it does not capture the last key typed

--- events.js
+++ events.js
@@ -15,12 +15,32 @@ var h1Hover = document.querySelector('h1');
 h1Hover.addEventListener('mouseover', hoverIn, false);
 h1Hover.addEventListener('mouseout', hoverOut, false);
 // Changes outputEl text when the mouse hovers on and out of the H1 Element
-function hoverIn (h1) {
-  outputEl.innerHTML = 'You\'re in the danger zone!';
+function hoverIn (h1Hover) {
+  outputEl.innerHTML = 'You moved your mouse over the header.';
 };
-function hoverOut (h1) {
-  outputEl.innerHTML = 'You\'ve left the danger zone!';
+function hoverOut (h1Hover) {
+  outputEl.innerHTML = 'You left me!! For a younger element, too!';
 };
+
+var userInput = document.getElementById('keypress-input');
+function getInputValue (input) {
+  input = input.value;
+  return input;
+};
+userInput.addEventListener('keypress', addtoDOM, false);
+function addtoDOM () {
+  var input = getInputValue(userInput);
+  outputEl.innerHTML = input;
+};
+
+// userInput.addEventListener('keypress', function() {
+//   userInput = userInput.value;
+
+// });
+// function mirror(userInput) {
+//   tapTap = userInput.value;
+//   outputEl += tapTap
+// };
 
   
 
